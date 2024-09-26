17ad96260da8e29aede2bc4d4db1faa1d4e85c48
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-26 10:11:54 -0500

    views

--- ptpetsadoptions-project/adoption/views.py
+++ ptpetsadoptions-project/adoption/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello It\'s a test</h1>')
+def pet_detail(request, pet_id):
+    return HttpResponse(f'<p>pet_detail view with pet_id {pet_id}</p>')
