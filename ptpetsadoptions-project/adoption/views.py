74aac9d517dac8d6e364a4c24c3b437a00ceb0f7
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-26 11:14:51 -0500

    templates - starting

--- ptpetsadoptions-project/adoption/views.py
+++ ptpetsadoptions-project/adoption/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Pet
 
 def home(request):
-    return HttpResponse('<h1>Hello It\'s a test</h1>')
+    pets = Pet.objects.all()
+
+    return render(request, 'home.html', {'pets': pets})
+
+    #return HttpResponse('<h1>Hello It\'s a test</h1>')
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with pet_id {pet_id}</p>')
+    try:    
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404("Pet is not found")
+    return render(request, 'pet_detail.html', {'pet': pet})
+
+    #return HttpResponse(f'<p>pet_detail view with pet_id {pet_id}</p>')
