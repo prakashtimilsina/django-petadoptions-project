349805b5e8d8cdb4ec49e851a6b17bd44385d729
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-09-27 11:38:22 -0500

    forms added

--- ptpetsadoptions-project/adoption/views.py
+++ ptpetsadoptions-project/adoption/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Pet
+from .forms import NameForm
+from django.core.mail import send_mail
+
+
+def get_name(request):
+    # if this is POST request, we need to process the form data
+    if request.method == 'POST':
+        # Create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+
+        #Check whether form is valid.
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+            # process the data in form.cleaned_data as required
+            # ...
+            # Redirect to a new URL.
+            recipients = ["prakashchitwan38@gmail.com"]
+            if cc_myself:
+                recipients.append(sender)
+            # send_mail(subject, message, sender, recipients)
+            print(f'{subject}, {message}, {sender}, {cc_myself}, {recipients}')
+            return HttpResponse(f'<h1>The email is sent to </h1>')
+    #if a GET (or any other method),  we will create a blank form
+    else:
+        form = NameForm()
+    return render(request, 'name.html', {'form': form})
+    # return render(request, 'form_test.html', {'current_name': 'Prakash Timilsina'})
+    
 
 def home(request):
     pets = Pet.objects.all()
--- ptpetsadoptions-project/ptpetsadoptions/urls.py
+++ ptpetsadoptions-project/ptpetsadoptions/urls.py
@@ -5,5 +5,6 @@ from adoption import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('form/', views.get_name, name='get-name'),
     path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
